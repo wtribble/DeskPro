d76645285c7dd08a7a2c9ec868ab168b8c713aa1
wtribble <westribble@gmail.com>
2021-03-22 19:48:25 -0500

    Add files via upload

---
+++ HueOccupiedv3.js
@@ -0,0 +1,24 @@
+import xapi from 'xapi';
+
+function hue(data) {
+  var url = 'http://<ip>/api/<user>/groups/1/action';
+  var headers = 'Content-Type: application/json';
+  var command = `{"on" : ` + data + `}`;
+  xapi.command('HttpClient Put', { 'Url': url, 'Header': headers }, command);
+}
+
+function occupiedTrigger(isOccupied) {
+  console.log('Occupied?', isOccupied);
+  if (isOccupied == 'Yes') {
+    hue('true');
+  }
+
+    else if (isOccupied == 'No') {
+      hue('false');
+    }
+}
+
+/**
+*Prints the occupied status to log and attempts to trigger the hue lights based on the status
+ */
+xapi.Status.RoomAnalytics.PeoplePresence.on(occupiedTrigger);
